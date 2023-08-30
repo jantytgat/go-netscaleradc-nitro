177446053e2a533671924dec8e4644bb6991c7b5
Jan Tytgat <jan.tytgat@corelayer.eu>
2023-08-30 21:40:32 +0200

    Added struct tags for ConnectionSettings
    Signed-off-by: Jan Tytgat <jan.tytgat@corelayer.eu>

--- pkg/nitro/connectionSettings.go
+++ pkg/nitro/connectionSettings.go
@@ -24,13 +24,13 @@ import (
 )
 
 type ConnectionSettings struct {
-	UseSsl                    bool
-	Timeout                   int
-	UserAgent                 string
-	ValidateServerCertificate bool
-	LogTlsSecrets             bool
-	LogTlsSecretsDestination  string
-	AutoLogin                 bool
+	UseSsl                    bool   `json:"useSsl" yaml:"useSsl" mapstructure:"useSsl"`
+	Timeout                   int    `json:"timeout" yaml:"timeout" mapstructure:"timeout"`
+	UserAgent                 string `json:"userAgent" yaml:"userAgent" mapstructure:"userAgent"`
+	ValidateServerCertificate bool   `json:"validateServerCertificate" yaml:"validateServerCertificate" mapstructure:"validateServerCertificate"`
+	LogTlsSecrets             bool   `json:"logTlsSecrets" yaml:"logTlsSecrets" mapstructure:"logTlsSecrets"`
+	LogTlsSecretsDestination  string `json:"logTlsSecretsDestination" yaml:"logTlsSecretsDestination" mapstructure:"logTlsSecretsDestination"`
+	AutoLogin                 bool   `json:"autoLogin" yaml:"autoLogin" mapstructure:"autoLogin"`
 }
 
 func (n *ConnectionSettings) GetUrlScheme() string {
