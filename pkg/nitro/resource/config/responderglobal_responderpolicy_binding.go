26cb4a1c8d9c07464e195bbb2fcd7b9f6094c9b4
Jan Tytgat <jan.tytgat@corelayer.eu>
2023-08-31 13:57:08 +0200

    Updated field type for FlowType in ResponderGlobalResponderPolicyBinding
    Signed-off-by: Jan Tytgat <jan.tytgat@corelayer.eu>

--- pkg/nitro/resource/config/responderglobal_responderpolicy_binding.go
+++ pkg/nitro/resource/config/responderglobal_responderpolicy_binding.go
@@ -26,7 +26,7 @@ type ResponderGlobalResponderPolicyBinding struct {
 	LabelName              string  `json:"labelname,omitempty" nitro:"permission=readwrite"`
 	Invoke                 bool    `json:"invoke,omitempty" nitro:"permission=readwrite"`
 	BoundPolicyNumber      float64 `json:"numpol,omitempty" nitro:"permission=readonly"`
-	FlowType               float64 `json:"flowtype,omitempty" nitro:"permission=readonly"`
+	FlowType               string  `json:"flowtype,omitempty" nitro:"permission=readonly"`
 	Count                  float64 `json:"__count,omitempty" nitro:"permission=readonly"`
 }
 
