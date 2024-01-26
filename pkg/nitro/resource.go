da260b5ed5e373d0815a7309109bc8445dd7891f
Jan Tytgat <jan.tytgat@corelayer.eu>
2024-01-26 22:51:11 +0100

    Update ResourcePackagePath

--- pkg/nitro/resource.go
+++ pkg/nitro/resource.go
@@ -17,5 +17,5 @@
 package nitro
 
 const (
-	ResourcePackagePath = "github.com/corelayer/netscaleradc-nitro-go/pkg/nitro/resource/"
+	ResourcePackagePath = "github.com/corelayer/go-netscaleradc-nitro/pkg/nitro/resource/"
 )
