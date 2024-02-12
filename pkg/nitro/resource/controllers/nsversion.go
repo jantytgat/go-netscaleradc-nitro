532736d6888ac92077c547e4c7a954fbf67fd31c
Jan Tytgat <jan.tytgat@corelayer.eu>
2024-02-12 23:13:25 +0100

    Add nsversion.go

---
+++ pkg/nitro/resource/controllers/nsversion.go
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2023 CoreLayer BV
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+package controllers
+
+import (
+	"net/http"
+
+	"github.com/corelayer/go-netscaleradc-nitro/pkg/nitro"
+	"github.com/corelayer/go-netscaleradc-nitro/pkg/nitro/resource/config"
+)
+
+type NsVersionController struct {
+	client *nitro.Client
+}
+
+func NewNsVersionController(client *nitro.Client) *NsVersionController {
+	c := NsVersionController{
+		client: client,
+	}
+	return &c
+}
+
+func (c *NsVersionController) Get(name string, attributes []string) (*nitro.Response[config.NsVersion], error) {
+	r := nitro.Request[config.NsVersion]{
+		Method:       http.MethodGet,
+		ResourceName: name,
+		Attributes:   attributes,
+	}
+	return nitro.ExecuteNitroRequest(c.client, &r)
+}
