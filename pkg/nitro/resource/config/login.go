51e4bbc8a002aa997b35656902abc8357e23bfbd
Jan Tytgat <jan.tytgat@corelayer.eu>
2024-12-09 23:22:56 +0100

    Remove copyright notice

--- pkg/nitro/resource/config/login.go
+++ pkg/nitro/resource/config/login.go
@@ -1,20 +1,6 @@
-/*
- * Copyright 2023 CoreLayer BV
- *
- *    Licensed under the Apache License, Version 2.0 (the "License");
- *    you may not use this file except in compliance with the License.
- *    You may obtain a copy of the License at
- *
- *        http://www.apache.org/licenses/LICENSE-2.0
- *
- *    Unless required by applicable law or agreed to in writing, software
- *    distributed under the License is distributed on an "AS IS" BASIS,
- *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *    See the License for the specific language governing permissions and
- *    limitations under the License.
- */
-
 package config
+
+import "log/slog"
 
 type Login struct {
 	Username string `json:"username,omitempty" nitro:"permission=readwrite"`
@@ -23,4 +9,9 @@ type Login struct {
 
 func (r Login) GetTypeName() string {
 	return "login"
+}
+
+func (r Login) LogValue() slog.Value {
+	return slog.GroupValue(
+		slog.String("username", r.Username))
 }
